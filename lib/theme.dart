b6f77eb9377ef9f3949f01b8b150b3cc9f6dcdc4
L4NYX <137173770+L4NYX@users.noreply.github.com>
2024-02-26 22:21:56 +0700

    Initial commit
    Ini adalah exam flutter saya

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+ import 'package:flutter/material.dart';
+
+ //Colors
+ Color green1 = const Color(0xFF05BE71);
+
+ //Typography
+ TextStyle header = const TextStyle(fontFamily: 'Montserrat', fontSize: 24);
+ TextStyle header_semibold = header.copyWith(fontWeight: FontWeight.w500);
+
+ TextStyle header_bold = header.copyWith(fontWeight: FontWeight.w700);
